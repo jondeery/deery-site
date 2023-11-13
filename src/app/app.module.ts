434f03b6a4ea0a6f67c83ce0694ac5d6b001b59f
jondeery <48105666+jondeery@users.noreply.github.com>
2023-11-13 13:57:38 -0500

    bouncing deery logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BouncingLogoComponent } from './bouncing-logo/bouncing-logo.component';
+import { BouncingLogo2Component } from './bouncing-logo2/bouncing-logo2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BouncingLogoComponent,
+    BouncingLogo2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bouncing-logo/bouncing-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bouncing-logo',
+  templateUrl: './bouncing-logo.component.html',
+  styleUrls: ['./bouncing-logo.component.scss']
+})
+export class BouncingLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
